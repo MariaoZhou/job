858c099ed7c30a9fa2aaa92d110e924d31bd0efc
xutie <xt@yuancore.com>
2017-08-18 15:50:32 +0800

    导入

--- src/main/java/com/admin/web/test/JobTest.java
+++ src/main/java/com/admin/web/test/JobTest.java
@@ -1,13 +1,18 @@
 package com.admin.web.test;
 
+import com.admin.web.model.City;
 import com.admin.web.model.Countries;
+import com.admin.web.model.JobInfo;
+import com.admin.web.model.UserInfo;
 import com.admin.web.util.DateUtils;
 import com.admin.web.util.excel.JobExcel;
+import com.rlax.web.model.Data;
 import org.jeecgframework.poi.excel.ExcelImportUtil;
 import org.jeecgframework.poi.excel.entity.ImportParams;
 import org.junit.Test;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.util.List;
 
 /**
@@ -19,20 +24,43 @@ public class JobTest {
     public void excel(){
 
         ImportParams params = new ImportParams();
-        List<JobExcel> list = ExcelImportUtil.importExcel(new File("D:/333.xlsx"), JobExcel.class, params);
-
-
+        List<JobExcel> list = null;
+        try {
+            list = ExcelImportUtil.importExcel(new FileInputStream("D:/111.xlsx"), JobExcel.class, params);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         for (JobExcel job : list){
-            //System.out.println(job.toString());
+            System.out.println(job.toString());
 
-          /*  String sql = String.format(
-                    "INSERT INTO `job`.`job_unit_position` " +
-                            "(`job`, `salary`, `unit`, `country`, `city`, `skill`, `street`, `jobDetail`, `tel`, `qualification`, `strength`, `createDate`) VALUES " +
-                            "('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s');",
-*/
-//                             job.getName(), job.getSalary(), job.getUnit(), job.getCountry(), job.getCity(),
-//                            job.getSkill(), job.getStreet(), job.getJobDetail(), job.getTel(), job.getQualification(), job.getStrength() , DateUtils.getDateTime());
 
+                JobInfo info = new JobInfo();
+
+                UserInfo user = UserInfo.dao.findById(job.getUserId());
+                info.setUserId(user.getId());
+                info.setUserName(user.getName());
+
+                City city = City.dao.findFirst("select * from j_city where name = ?" , job.getCityName());
+                info.setCountriesId(city.getCountriesId());
+                info.setCountriesName(city.getCountriesName());
+                info.setCityId(city.getId());
+                info.setCityName(city.getName());
+
+                info.setCompanyName(job.getCompanyName());
+                info.setTitle(job.getTitle());
+                info.setTel(job.getTel());
+
+                info.setJobTypeName(job.getJobTypeName());
+                info.setJobRequirementsName(job.getJobRequirementsName());
+                info.setJobWelfareName(job.getJobWelfareName());
+                info.setJobNatureName(job.getJobNatureName());
+
+                info.setJobSalaryName(job.getJobSalaryName());
+                String salaryOrder = Data.dao.getCodeDescByCodeAndType(job.getJobSalaryName(),"JOB_SALARY");
+                info.setJobSalaryOrder(Integer.parseInt(salaryOrder));
+
+
+                info.save();
 
             //System.out.println(sql);
         }
