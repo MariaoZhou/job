e37e18d262859c57125ea03f04c7c6dea2b68bd8
漂途 <514662144@qq.com>
2017-09-08 22:36:19 +0800

    统计查询

---
+++ src/main/java/com/admin/web/controller/job/JobReportController.java
@@ -0,0 +1,48 @@
+package com.admin.web.controller.job;
+
+import com.admin.web.base.BaseBussinessController;
+import com.jfinal.ext.route.ControllerBind;
+import com.jfinal.plugin.activerecord.Db;
+import com.jfinal.plugin.activerecord.Record;
+import com.rlax.web.model.Data;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 职位统计
+ */
+@ControllerBind(controllerKey = "/job/report")
+public class JobReportController extends BaseBussinessController {
+
+	public void index() {
+
+        String data = Data.dao.getCodeByCodeDescAndType("REPORT_KEY", "统计关键字");
+        String[] keys = data.split(",");
+
+
+        String sql = "SELECT count(j.title) as 'report' FROM j_job_info j where instr(j.title, ?);";
+        List<Map<String, String>> reportList = new ArrayList<>();
+        for (String key : keys){
+            Map<String, String> report = new HashMap<>();
+            Record record = Db.findFirst(sql , key);
+            report.put("key", key);
+            report.put("num", record.get("report"));
+            reportList.add(report);
+
+        }
+
+		keepPara();
+		setAttr("list", reportList);
+		render("main.html");
+	}
+
+	@Override
+	public void onExceptionError(Exception e) {
+		setSessionErrorMessage(e.getMessage());
+		redirect("/job/report");
+	}
+
+}
